9d1d67d6c55b5b4b065c8cb143efa31b70f347b1
Gautam-Rathour <gau321tam@gmail.com>
2026-09-11 13:09:08 +0530

    adding old code

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -1,13 +1,40 @@
+import React from "react";
 
+function LeftSection({
+  imageURL,
+  productName,
+  productDesription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
+  return (
+    <div className="container mt-5">
+      <div className="row p-5">
+        <div className="col-5 p-5">
+          <img src={imageURL} />
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDesription}</p>
+          <div style={{textDecoration: "none"}}>
+            <a href={tryDemo}>Try Demo</a>
+            <a href={learnMore} style={{ marginLeft: "50px" }}>Learn More</a>
+          </div>
 
-import React from 'react';
-
-function LeftSection() {
-    return (
-        <h1>LeftSection</h1>
-    );
+          <div className="mt-5">
+            <a href={googlePlay}>
+                <img src="/media/image/google-play-badge.svg" />
+            </a>
+            <a href={appStore}>
+                <img src="/media/image/appstore-badge.svg"  style={{ marginLeft: "50px" }} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
 export default LeftSection;
-
--- frontend/src/landing_page/products/ProductsPage.js
+++ frontend/src/landing_page/products/ProductsPage.js
@@ -1,20 +1,28 @@
 import React from "react";
 import Navbar from "../Navbar";
+import Hero from "./Hero";
 import LeftSection from "./LeftSection";
 import RightSection from "./RightSection";
+import Universe from "./Universe";
 import Footer from "../Footer";
 
-
-
-function ProductsPage() {
-    return (
-        <>
-
-            <LeftSection />
-            <RightSection />
-
-        </>
-    )
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <LeftSection
+        imageURL="media/image/kite3-dashboard.png"
+        productName="Kite by Zerodha"
+        productDesription="Kite is a sleek investment and trading platform built for modern times and sensibilities. Ground breaking innovations presented with excellent usability, investing in the stock markets has never been this easy. Really."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection />
+      <Universe />
+    </>
+  );
 }
 
-export default ProductsPage;
+export default PricingPage;
